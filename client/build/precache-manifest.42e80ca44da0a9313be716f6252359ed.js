e8a189bd63144624da1c009005838c0445cde405
Mesut Sipar <mesutsipar@gmail.com>
2020-05-19 18:36:38 -0500

    adding assignment

---
+++ client/build/precache-manifest.42e80ca44da0a9313be716f6252359ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8951190ebf44b29bffe",
+    "url": "/static/css/main.8990060d.chunk.css"
+  },
+  {
+    "revision": "d8951190ebf44b29bffe",
+    "url": "/static/js/main.d8951190.chunk.js"
+  },
+  {
+    "revision": "7b22e4ef3ca28f18c721",
+    "url": "/static/js/1.7b22e4ef.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "5443ea44b1b01de9dbf3f2aafdab0bdc",
+    "url": "/index.html"
+  }
+];
